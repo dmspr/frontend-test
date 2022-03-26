236f9a028bf0ab66d1af1a5adc2513c489163b8a
Dimas Prasetyo <94463877+dmspr@users.noreply.github.com>
2022-03-26 23:06:56 +0700

    Delete App.test.js

--- src/__test__/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import App from "../App";
-
-test('renders "React Axios example"', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/react axios example/i);
-  expect(linkElement).toBeInTheDocument();
-});
